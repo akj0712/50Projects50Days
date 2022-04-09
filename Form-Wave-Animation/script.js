a63565f5a2217aee92d309ab9aad9bec7501218e
Abhinav Kumar Jha <akj0712@duck.com>
2022-04-09 16:25:26 +0530

    Form Wave Animation

---
+++ Form-Wave-Animation/script.js
@@ -0,0 +1,13 @@
+console.log("Form Wave Animation");
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+    label.innerHTML = label.innerText
+        .split("")
+        .map(
+            (letter, idx) =>
+                `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`
+        )
+        .join("");
+});
