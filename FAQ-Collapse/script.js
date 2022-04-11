25827759c877a0c1625d378c5e2bd8a1112178ff
Abhinav Kumar Jha <akj0712@duck.com>
2022-04-11 14:45:38 +0530

    FAQ Collapse

---
+++ FAQ-Collapse/script.js
@@ -0,0 +1,9 @@
+console.log("FAQ-Collapse");
+
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+        toggle.parentNode.classList.toggle("active");
+    });
+});
