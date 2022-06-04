c4ede0ffc94d8f7eb74666fc29cf5b41bd3e5e41
Abhinav Kumar Jha <akj0712@duck.com>
2022-06-04 15:19:21 +0530

    Sticky Navbar

---
+++ Sticky-Navbar/script.js
@@ -0,0 +1,13 @@
+console.log("Sticky Navbar");
+
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  // console.log(window.scrollY, nav.offsetHeight);
+  if (window.scrollY >= nav.offsetHeight + 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
