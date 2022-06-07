a8514837d342d1720a6637a7b54e31bfe48e8350
Abhinav Kumar Jha <akj0712@duck.com>
2022-06-07 23:50:11 +0530

    Toast Notification

---
+++ Toast-Notification/script.js
@@ -0,0 +1,31 @@
+console.log("Toast Notification");
+
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = ["Message One", "Message Two", "Message Three", "Message Two"];
+
+const types = ["info", "success", "error"];
+
+button.addEventListener("click", () => createNotification());
+
+function createNotification(message = null, type = null) {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+  notif.classList.add(type ? type : getRandomTypes());
+  notif.innerText = message ? message : getRandomMessage();
+
+  toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 2000);
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomTypes() {
+  return types[Math.floor(Math.random() * types.length)];
+}
